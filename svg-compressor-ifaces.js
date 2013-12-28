73782691576abfe9e0a714c52b84902c7552cd00
Vadim Nikiforov <vnikiforov@gmail.com>
2013-12-28 21:26:12 +0400

    Add base data types

--- svg-compressor-ifaces.js
+++ svg-compressor-ifaces.js
@@ -2,12 +2,33 @@
 
 // Main data types
 
+// Collection of group transformation
+var TransformCollection; 
+
+class SkippedElement
+{
+	var Name; // Human orientated string element description.
+	var Reason; // Why element skipped.
+	var Advice; // Waht user can do for fix.
+} 
+
+
+// Result of compess group
+class CompressedSVG
+{
+	var PathTree;
+	var SkippedObjects; // Array of SkippedElement
+}
 
 
 // Main function
-function svg_compress(rootGroupNode)
+function svg_compress(rootGroupTree)
 {
-	var resultTree;
+	var resultTree = new CompressedSVG();
+
+	// 
+
 
 	return resultTree;
 }
+
