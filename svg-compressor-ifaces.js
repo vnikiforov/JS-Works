aedef4080c6fafa7e448ee6d36140ff4f7cfed60
Vadim Nikiforov <vnikiforov@gmail.com>
2014-01-01 20:04:24 +0400

    Added doc files

--- svg-compressor-ifaces.js
+++ svg-compressor-ifaces.js
@@ -1,28 +1,5 @@
+// Creates correct comperessed gliph file.
+// dataFile - data file
+function svg_compress(dataFile);
 
-class SkippedElement
-{
-	var Name; // Human orientated string element description.
-	var Reason; // Why element skipped.
-	var Advice; // Waht user can do for fix.
-} 
-
-
-// Result of compess group
-class CompressedSVG
-{
-	var PathTree;
-	var SkippedObjects; // Array of SkippedElement
-}
-
-
-// Main function
-// rootGroupTree - <g> - node for compress.
-function svg_compress(rootGroupTree)
-{
-	var resultTree = new CompressedSVG();
-
-	// Process...
-
-	return resultTree;
-}
 
